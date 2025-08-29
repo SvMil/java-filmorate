794be45051d486f89a867184f7be9045aa6f82f5
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2025-08-29 08:20:32 +0300

    add controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -16,13 +16,14 @@ import java.util.Map;
 @RequestMapping("/users")
 public class UserController {
     private final Map<Long, User> users = new HashMap<>();
+
     @GetMapping
-    public Collection<User> findAll(){
+    public Collection<User> findAll() {
         return users.values();
     }
 
     @PostMapping
-    public User  create(@RequestBody User user) {
+    public User create(@RequestBody User user) {
         // проверяем выполнение необходимых условий
         if (user.getLogin() == null || user.getLogin().isBlank()) {
             throw new ConditionsNotMetException("Логин не может быть пустым");
@@ -36,7 +37,7 @@ public class UserController {
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
-          if (user.getBirthday().isAfter(LocalDate.now())) {
+        if (user.getBirthday().isAfter(LocalDate.now())) {
             throw new ConditionsNotMetException("Дата рождения не может быть в будущем");
         }
         user.setId(getNextId());
