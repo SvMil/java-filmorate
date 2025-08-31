66f5c0fe5c480306bb353c615fbed0582d44236f
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2025-08-31 23:56:35 +0300

    fix controllers-films-users

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.exception.*;
