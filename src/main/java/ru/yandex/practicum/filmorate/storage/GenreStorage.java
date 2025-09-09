46ca09949a61c62aa2a543861198eb990f16f75a
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2025-09-10 01:32:04 +0300

    fix add-database branch

--- src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.Map;
-import java.util.Optional;
 
 @Component("GenreStorage")
 public interface GenreStorage {
