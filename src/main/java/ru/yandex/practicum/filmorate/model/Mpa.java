3153cc7e5b0cda7c23368b14aa8ecc7c31bf3397
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2025-09-10 01:57:59 +0300

    fix add-database branch

--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -4,6 +4,11 @@ import lombok.Data;
 
 @Data
 public class Mpa {
-    private final Integer id;
-    private final String name;
+    private Integer id;
+    private String name;
+
+    public Mpa(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
