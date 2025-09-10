bebe0b5bcd939c95bc29bf23ac285807d5f89b5f
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2025-09-10 15:18:07 +0300

    fix add-database branch

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -3,9 +3,9 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dao.GenreDbStorage;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.storage.GenreStorage;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -13,10 +13,10 @@ import java.util.Collections;
 @Service
 @Slf4j
 public class GenreService {
-    private final GenreStorage genreStorage;
+    private final GenreDbStorage genreStorage;
 
     @Autowired
-    public GenreService(GenreStorage genreStorage) {
+    public GenreService(GenreDbStorage genreStorage) {
         this.genreStorage = genreStorage;
     }
 
