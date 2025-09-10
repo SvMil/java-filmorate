08d5fa8181c5ff204d4408cfb0d373fc9f940f5c
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2025-09-10 16:53:21 +0300

    fix add-database branch

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,3 @@
-INSERT INTO PUBLIC.GENRE
-(GENRE_NAME) VALUES ('Комедия'),
-                    ('Драма'),
-                    ('Мультфильм'),
-                    ('Триллер'),
-                    ('Документальный'),
-                    ('Боевик');
-
 INSERT INTO PUBLIC.MPA(RATING) VALUES   ('G'),
                                         ('PG'),
                                         ('PG-13'),
