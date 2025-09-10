41fb8d008eaf20333bf0b2a9e3c172f1936b896f
milovidova.s.e <Milovidova.S.E@sbertech.ru>
2025-09-10 23:46:49 +0300

    fix add-database branch

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +15,9 @@ import java.util.Collection;
 @RestController
 @Validated
 @RequestMapping("/mpa")
+@RequiredArgsConstructor
 public class MpaController {
     private final MpaService mpaService;
-
-    @Autowired
-    public MpaController(MpaService mpaService) {
-        this.mpaService = mpaService;
-    }
 
     @GetMapping
     public Collection<Mpa> getAllMpa() {
